e2bb0d0d3742a446db13428c3c8d2dc9860cae3b
gustaacoder <gustavosantos01.contato@gmail.com>
2026-02-10 17:16:39 -0300

    Add plugins and cursor animated

--- lua/plugins/animations.lua
+++ lua/plugins/animations.lua
@@ -3,7 +3,7 @@ return {
     "sphamba/smear-cursor.nvim",
     event = "VeryLazy",
     opts = {
-      cursor_color = "#00d4ff",
+      cursor_color = "#00ff17",
       stiffness = 0.6,
       trailing_stiffness = 0.3,
       hide_target_hack = false,
---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    opts = {
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-CR>",
+      },
+      disable_inline_completion = false,
+    },
+  },
+}
